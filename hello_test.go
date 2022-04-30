dfba84c26f65893d93b071ae751710e3c9884b56
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-04-30 12:15:59 +0100

    Add language variable to Hello function

--- hello_test.go
+++ hello_test.go
@@ -11,14 +11,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Saying hello to people", func(t *testing.T) {
-		got := Hello("Jazzy")
+		got := Hello("Jazzy", "")
 		want := "Hello, Jazzy"
 		assertCorrectMessage(t, got, want)
 
 	})
 	t.Run("Say 'Hello World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+
+	})
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+
+	})
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 		assertCorrectMessage(t, got, want)
 
 	})
