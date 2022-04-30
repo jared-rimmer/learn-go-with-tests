e3da5afc466dc52b5933d58b7db6da97ac16a578
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-04-30 15:57:47 +0100

    Add completed array module with tests

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,28 @@ func SumArray(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, SumArray(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, SumArray(tail))
+		}
+	}
+
+	return sums
+}
