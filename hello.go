29bb9689b09176922f00862187fa55a7ed6363fb
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-04-30 12:30:09 +0100

    Refactor Hello function by splitting out functionality

--- hello.go
+++ hello.go
@@ -16,8 +16,10 @@ func Hello(name, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
@@ -25,9 +27,11 @@ func Hello(name, language string) string {
 		prefix = spanishHelloPrefix
 	case hebrew:
 		prefix = hebrewHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name
+	return
 }
 
 func main() {
